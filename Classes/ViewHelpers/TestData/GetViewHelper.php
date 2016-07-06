f8326fab9fea1fa13595f09faddb04ec2a29f9f4
Dennis Römmich <tild@youthweb.net>
2016-07-06 18:30:10 +0200

    Added GetViewhelper

--- Classes/ViewHelpers/TestData/GetViewHelper.php
+++ Classes/ViewHelpers/TestData/GetViewHelper.php
@@ -1,17 +1,18 @@
 <?php
-namespace Dennis\Seeder\Tests\Main;
+namespace Dennis\Seeder\ViewHelpers\TestData;
 
 /***************************************************************
+ *
  *  Copyright notice
  *
- *  (c) 2016 Dennis Römmich <dennis@roemmich.eu>
+ *  (c) 2016 Dennis Römmich <dennis.roemmich@sunzinet.com>, sunzinet AG
  *
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  The GNU General Public License can be found at
@@ -24,24 +25,35 @@ namespace Dennis\Seeder\Tests\Main;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
-use TYPO3\CMS\Core\Tests\UnitTestCase;
+use Dennis\Seeder\Provider\Faker;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
- * Test case for class \Dennis\Seeder\Tests\Main\TestTest
+ * Class GetViewHelper
  *
- * @author Dennis Römmich <dennis@roemmich.eu>
+ * @package Dennis\Seeder\ViewHelper\GetViewHelper
  */
-class TestTest extends UnitTestCase
+class GetViewHelper extends AbstractViewHelper
 {
+    /**
+     * @var Faker
+     */
+    protected $faker;
 
     /**
-     * testsCanBeExecuted
-     *
-     * @test
-     * @return void
+     * GetViewHelper constructor.
      */
-    public function testsCanBeExecuted()
+    public function initialize()
     {
-        $this->assertSame(true, true);
+        $faker = \Faker\Factory::create();
+        $this->faker = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(Faker::class, $faker);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function render()
+    {
+        return $this->faker->get($this->renderChildren());
     }
 }
