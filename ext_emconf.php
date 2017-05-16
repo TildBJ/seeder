ae1cf3e01a7597afd57646eb5f6b9a02dbf2ff06
Dennis Römmich <Dennis.Roemmich@sunzinet.com>
2017-05-16 17:07:31 +0200

    [TASK] Add TYPO3 8.7 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.1.1',
+    'version' => '0.1.2',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.3.99',
+            'typo3' => '7.6.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
