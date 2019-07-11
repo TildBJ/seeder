0eafeae11424d0a9ba1b66d072eaee4d6fa3a670
Dennis Römmich <dennis.roemmich@sunzinet.com>
2019-07-11 18:13:21 +0200

    [FEATURE] Add TYPO3 9.5 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.3.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '7.6.0-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
